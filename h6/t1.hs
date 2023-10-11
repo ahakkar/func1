0f2236385f976b2f3e83dd56daa276c35e11306e
Antti Hakkarainen <a.hakkarainen@iki.fi>
2023-10-11 22:37:13 +0300

    h6 solutions

--- h6/t1.hs
+++ h6/t1.hs
@@ -1,1 +1,21 @@
 -- func1 AH autumn 2023
+
+validate :: String -> Bool
+validate str 
+  | length str /= 18            = False
+  | take 2 str /= "FI"          = False
+  | not (all isDigit restStr)   = False
+  | otherwise                   = checkIBAN str
+  where
+    isDigit c = c >= '0' && c <= '9'
+    restStr = take 16 (drop 2 str)
+
+-- strtoi(str) % 97 should be 1...
+checkIBAN :: String -> Bool
+checkIBAN str = read(replaceChars str) `mod` 97 == 1
+
+-- drop first four, add 1518 to end, add 3rd and 4th dropped char to end of str
+replaceChars :: String -> String
+replaceChars str = drop 4 str ++ "1518" ++ take 2 (drop 2 str)
+
+
--- h6/t2.hs
+++ h6/t2.hs
@@ -1,1 +1,7 @@
 -- func1 AH autumn 2023
+
+distanceFilter :: (String -> String -> Float) -> Float -> String -> [String] -> [String]
+distanceFilter f d s ss = filter (\x -> f s x <= d) ss
+
+distance3 :: String -> String -> Float
+distance3 x y = fromIntegral $ abs $ length x - length y
